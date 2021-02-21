2050bbe3aaab35163c7d374b847184dd2709b049
guest-P0R6IZ89 <alamsawame@hotmail.com>
2021-02-21 21:52:20 +0900

    extract callendar data

---
+++ secrets.py
@@ -0,0 +1,6 @@
+USER_ID = '01243111'
+USER_PASSWORD = '04180418'
+BASE_URL = 'https://wps.fastretailing.com/'
+TAB_LIST = ['', 'information', 'calendar', 'summary']
+TIMELINE_ALERT_TYPE = ['Shift fixed', 'Post notification', 'Shift adjustment', 'Shift draft']
+TIMELINE_XPATH = '/html/body/div[2]/div[1]/div[1]/div/section/ng-include[2]'
--- test.py
+++ test.py
@@ -7,8 +7,11 @@ current_time = datetime.datetime.now()
 print(current_time.day)
 
 print(monthrange(current_time.year, 1))
-
-for r in range(1, 6):
-    print(r)
-
-print('S{0}-{1}-{0}'.format('me', 'I'))
+for n in range(10, 12):
+    for r in range(1, 6):
+        if r == 4:
+            # r += 1
+            break
+        print(n)
+        print(r)
+        
